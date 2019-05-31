7bdd4eb8b03a6f6844b6c07e2ae78d1040924e1b
Taurin190 <taura.koichi@gmail.com>
2019-05-31 09:36:00 +0900

    add popup menu for list view to show logout menu

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todo_flutter/ui/todo_list.dart';
 import 'package:todo_flutter/entity/todo_hub.dart';
+import 'package:todo_flutter/entity/choice.dart';
 import 'package:todo_flutter/bloc/app_bloc.dart';
 import 'package:todo_flutter/bloc/todo_bloc.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
@@ -31,6 +32,19 @@ class ToDoHomeState extends State<TodoHome> {
     centerTitle: true,
     elevation: 1.0,
     title: Text("TODO List"),
+    actions: <Widget>[
+      PopupMenuButton<Choice>(
+        onSelected: null,
+        itemBuilder: (BuildContext context) {
+          return choices.map((Choice choice) {
+            return PopupMenuItem<Choice>(
+              value: choice,
+              child: Text(choice.title),
+            );
+          }).toList();
+        }
+      )
+    ],
   );
 
   @override
