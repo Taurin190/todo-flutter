6f492c86d5aa736fc75e9b735aaade5d3bc0c49e
Koichi Taura <taura.koichi@gmail.com>
2019-06-01 23:40:44 +0900

    add some firebase auth settings

--- lib/api/auth_api.dart
+++ lib/api/auth_api.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 class AuthApi {
+
+  final _auth = FirebaseAuth.instance;
   Future<bool> login(String username, String password) async {
     return await true;
   }
